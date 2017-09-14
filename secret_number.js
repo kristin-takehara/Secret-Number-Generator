eddcca68ab71cf5635d54348e103dbc725b7574b
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-13 15:19:01 -1000

    3 tests passing

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,7 @@
-'use strict';
-module.exports = function() {
-  
+//'use strict';
+module.exports = function () {
+ var secretNumber = (Math.floor(Math.random() * 1000000 + 1)); //generates a random number between 0 and 1000000
+  return function(){ //call the anonymous function once
+    return secretNumber; //returns the random number generated in the above call
+  };
 };
