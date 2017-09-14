8462badc04f4d0c62dcfbfcc0ebdc3e09b17314e
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-13 16:07:29 -1000

    math random FIXED

--- secret_number.js
+++ secret_number.js
@@ -1,6 +1,6 @@
 // 'use strict';
 module.exports = function() {
-   var secretNumber = (Math.floor(Math.random() * 1000000 + 1)); //generates a random number between 0 and 1000000
+   var secretNumber = (Math.floor(Math.random() * 1000001)); //generates a random number between 0 and 1000000
   return function(){ //call the anonymous function once
     return secretNumber; //returns the random number generated in the above call
   };
